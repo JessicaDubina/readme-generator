7fc3b3fb19ea0edd63063948a220097d6518c195
jdub <jessicaclaing@gmail.com>
2024-01-18 19:49:31 -0800

    added matrix of question prompts

--- index.js
+++ index.js
@@ -3,7 +3,60 @@ let inquirer = require('inquirer');
 let fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+inquirer.prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please give a brief description of the project",
+    },
+    {
+        type: "confirm",
+        name: "instCheck",
+        message: "Are there any installation instructions?",
+    },
+    {
+        type: "editor",
+        name: "installation",
+        message: "Please enter the installation instructions.",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide any relevant usage information.",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Choose a license.",
+        options: ["MIT", "Apache-2.0", "GPL-3.0","BSD-2-Clause","BSD-3-Clause","BSD-4-Clause", "UNLICENCED"],
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "What are the contribution guidelines?",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please provide any test instructions.",
+    },
+    {
+        type:,
+        name: "username",
+        message: "What is your Github username?",
+    },
+    {
+        type:,
+        name: "email",
+        message: "Please enter your email address",
+    },
+])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
