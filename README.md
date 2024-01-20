90c562ca8edfc855406f3dd26cdead5c2ca849c3
jdub <jessicaclaing@gmail.com>
2024-01-20 10:25:46 -0800

    created init function. updated readme and added sections, and installation info

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # readme-generator
+
+## Description
 A command-line app that generates a README.md based on prompted input from the user
+
+## Installation
+This app uses Inquirer 6.5.0.
+Run npm install to ensure you have the package before running this app
+
+## Usage
+
+## Credits
+N/A
+
+## License
+Please refer tot he license attached to the projects
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// TODO: Include packages needed for this application
+//adding packages needed
 let inquirer = require('inquirer');
 let fs = require('fs');
 
-//TODO: Make a text outline to populate with the answers
+//readme.me outline to populate with answers
 const generateReadme = ({title, description, installation, usage, license, contributing, tests, username, email}) => 
     `# ${title}
     
-## Description
+### Description
 ${description}
 
-## Table of Contents
+### Table of Contents
 [Installation](#installation)\n
 [Usage](#usage)\n
 [License](#license)\n
@@ -23,7 +23,7 @@ ${installation}
 ## Usage
 ${usage}
 
-## Licence
+## License
 This project is covered under the ${license} license
 
 ## Contributing
@@ -36,82 +36,75 @@ ${tests}
 Github: ${username}\n
 For additional information, please contact me at ${email}`;
 
-// const questions = [];
-inquirer.prompt([
-    {
-        type: "input",
-        name: "title",
-        message: "What is the title of your project?",
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "Please give a brief description of the project",
-    },
-    {
-        type: "confirm",
-        name: "instCheck",
-        message: "Are there any installation instructions?",
-    },
-    // {
-    //     type: "editor",
-    //     name: "installation",
-    //     message: "Please enter the installation instructions.",
-    // },
-    {
-        type: "input",
-        name: "usage",
-        message: "Please provide any relevant usage information.",
-    },
-    {
-        type: "list",
-        name: "license",
-        message: "Choose a license.",
-        choices: ["MIT", "Apache-2.0", "GPL-3.0","BSD-2-Clause","BSD-3-Clause","BSD-4-Clause", "UNLICENCED"],
-    },
-    {
-        type: "input",
-        name: "contributing",
-        message: "What are the contribution guidelines?",
-    },
-    {
-        type: "input",
-        name: "tests",
-        message: "Please provide any test instructions.",
-    },
-    {
-        type: "input",
-        name: "username",
-        message: "What is your Github username?",
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Please enter your email address",
-    },
-])
-.then((answers) => {
-    console.log(answers);
-    const readmePage = generateReadme(answers);
-    console.log(readmePage);
+//questions for the user
+const init = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please give a brief description of the project",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Please enter the installation instructions.",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please provide any relevant usage information.",
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Choose a license.",
+            choices: ["MIT", "Apache-2.0", "GPL-3.0","BSD-2-Clause","BSD-3-Clause","BSD-4-Clause", "UNLICENCED"],
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "What are the contribution guidelines?",
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Please provide any test instructions.",
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "What is your Github username?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email address",
+        },
+    ])
+    .then((answers) => {
+        //populate the readme outline with the given answers
+        const readmePage = generateReadme(answers);
 
-    // fs.writeFile('README.md', readmePage, (err) => {
+        //write readme file and give message to confirm success, or if there was an error
+        fs.writeFile(`${answers.title}.md`, readmePage, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("Generated new README.md");
+            }
+        }) 
+    })
+    .catch((error) => {
+        console.log("Something went wrong. Please try again.");
+        console.log(error);
+    })
+}
 
-    fs.writeFile(`${answers.title}.md`, readmePage, (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Generated new README.md");
-        }
-    }) 
-})
-.catch((error) => {
-    console.log("Something went wrong. Please try again.");
-    console.log(error);
-})
+//initialize the app
+init ();
 
-// TODO: Create a function to initialize app
-// function init() {}
-
-// // Function call to initialize app
-// init();
